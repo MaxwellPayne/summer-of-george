2ee550cf4af4173626f0c57e67a7b6130ebcd516
MaxwellPayne <mpmclenn@indiana.edu>
2014-08-01 04:20:28 -0300

    Updated package.json to depend on mongoose-private-paths. Began reworking RoundSchema to have private fields that can only be accessed by virtuals. Also created an 'immutable' Boolean field that prevents saving if true. immutable is switched to true the first time that a model is saved with over=true.

--- packages/frolf/server/models/round.js
+++ packages/frolf/server/models/round.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var mongoose = require('mongoose')
+    , mongoosePrivatePaths = require('mongoose-private-paths')
     , _ = require('underscore')
     , env = process.env.NODE_ENV || 'development'
     //, config = require('../config')
@@ -14,26 +15,19 @@ var RoundSchemaSetters = {
     setCourse: function(courseVal) {
 	// enforces referential integrity between
 	// performance: [{ hole: hole }] and courseVal.holes
-	if(_.any(this.performances, function(performance) {
-		return performance.hasOwnProperty('score');
-	    }))
-	{
-	// if attempting to change course and scores already
-	// exist for previous course, raise exception
-	    throw Error('must delete old scores before changing the course');
-	}
-	else
-	{
-	    // make this.performances' holes match the course
-	    this.performances = [];
-	    var performances = this.performances;
-	    _.forEach(courseVal.holes, function(hole) {
-		performances.push({ hole: hole });
-	    });
-	}
+	
+	// make this.performances' holes match the course
+	this.performances = [];
+	var performances = this.performances;
+	_.forEach(courseVal.holes, function(hole) {
+	    performances.push({ hole: hole });
+	});
 	return courseVal;
-    }
+    },
+	
 };
+
+
 
 var RoundSchema = new Schema({
 // A party of Players record HolePerformances
@@ -47,15 +41,59 @@ var RoundSchema = new Schema({
 	type: Schema.ObjectId,
 	ref: 'Player'
     },
-    performances: [{
-	hole: {
-	    type: Schema.ObjectId,
-	    ref: 'Hole'
-	},
-	score: {
-	    type: Number
-	}
-    }],
+    performances: {
+	type: [{
+	    hole: {
+		type: Schema.ObjectId,
+		ref: 'Hole'
+	    },
+	    score: {
+		type: Number
+	    }
+	}],
+	private: true
+    },
+    over: {
+	type: Boolean,
+	default: false,
+	private: true
+    },
+   immutable: {
+       // only used by the pre('save') event
+       // false until Round saved as over: true for first time
+       type: Boolean,
+       default: false,
+       private: true
+   }
+});
+
+RoundSchema.plugin(mongoosePrivatePaths);
+
+RoundSchema.methods.submitRound = function() {
+    if (this.over) throw new Error('Scorecard has already been submit');
+    var unscored = _.find(this.performances, function(performance) {
+	// check if any performances haven't been scored
+	return !performance.hasOwnProperty('score')
+    });
+    if (unscored) {
+	throw new Error('Hole {0} has no score'.format(unscored.hole.flagNumber));
+    }
+    this.over = true;
+};
+
+RoundSchema.virtual('isOver')
+    .get(function() {
+	return this.over;
+    });
+
+RoundSchema.pre('save', function(next) {
+    // If the Round is immutable, raise error pre save
+    // Else save and make immutable
+    if (this.over) next(new Error('This round is already over and therefore immutable'));
+    else {
+	this.immutable = true;
+	next();
+    }
 });
 
 
